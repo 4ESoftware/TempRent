f616ae89cfd6dadbd5b254a1f1629a78f55c446d
Radu Lupaescu <radu.lupaescu@edisolutions.ro>
2020-06-24 20:09:57 +0300

    fixed missing user type after social login

---
+++ service/src/AppBundle/Security/Authentication/Handler/AuthenticationSuccessHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Security\Authentication\Handler;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class AuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        return new RedirectResponse($this->router->generate('fos_user_profile'));
+    }
+}
